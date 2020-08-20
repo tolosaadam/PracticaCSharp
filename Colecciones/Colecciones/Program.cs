9aaed716d33e02d6982422ac686d0872afb8fe77
Adam <Adam@DESKTOP-GM6IUPR>
2020-08-19 21:36:10 -0300

    Upgradeando repos

---
+++ Colecciones/Colecciones/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Las colecciones tipo "List" nos permiten *Ordenar *Añadir *Eliminar *Buscar etc, elementos */
+
+            List<int> numeros = new List<int>();   //Declarando una lista
+
+            numeros.Add(5);
+            numeros.Add(6);
+            numeros.Add(7);
+
+            int[] listaNumeros = new int[] { 3, 6, 8, 10, 50 };    // Array convencional
+
+            for (int i = 0; i < 5; i++)
+            {
+                numeros.Add(listaNumeros[i]);   // Agregando a la lista los numeros que guarde antes en el array
+            }
+
+
+            for (int i = 0; i < numeros.Count ; i++)  //Recorro la lista y muestro los elementos del interior
+            {
+                Console.WriteLine(numeros[i]);
+            }
+
+            
+            // AGREGANDO ELEMENTOS  POR PANTALLA 
+            Console.WriteLine("Cuantos elementos quieres introducir?");
+            int cantElem = int.Parse(Console.ReadLine());
+            for(int i = 0; i < cantElem; i++)
+            {
+                Console.WriteLine($"Introduce el {i+1} elemento");
+                numeros.Add(int.Parse(Console.ReadLine()));
+            }
+                Console.WriteLine("\nElementos introducidos: ");
+            for (int i = 0; i < numeros.Count; i++)  //Recorro la lista y muestro los elementos del interior
+            {
+                Console.WriteLine(numeros[i]);
+            }
+
+            //USANDO FOR EACH CON COLLECTIONS "List"
+
+            Console.WriteLine("Introduce elementos en la coleccion (escribe '0' para salir)");
+            int condicion = 1;
+            while(condicion != 0)
+            {
+                condicion = int.Parse(Console.ReadLine());
+                numeros.Add(condicion);
+
+            }
+
+            numeros.RemoveAt(numeros.Count-1); /* Borramos el ultimo valor de la lista (para que no se vea el 0 de la condicion
+                                                * del while ) */
+            
+            Console.WriteLine("Elementos introducidos: ");
+
+            foreach(int elemento in numeros)
+            {
+                Console.WriteLine(elemento);
+            }
+        
+        
+        
+        }
+    }
+}
---
+++ Colecciones/Collections_LinkedList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> numeros = new LinkedList<int>();
+
+            LinkedListNode<int> nodoImportante = new LinkedListNode<int>(15);  /* Creo un nodo de valor '15' */
+            numeros.AddFirst(nodoImportante);                                   // Y lo agrego a la listay
+            
+            foreach(int num in new int[] { 10, 8, 6, 4, 2, 0 }) /* Uso un foreach para agregar numeros de un array que cree
+                                                                 * dentro del foreach a la 'LinkedList' que inicialice antes "numeros" */
+            {
+                numeros.AddFirst(num);    /* 'AddFirst' Va agregando los valores en el primer valor de posicion del array, osea
+                                           * que a medida que se van agregando nuevos valores, los que ya estaban van bajando
+                                           * de posicion
+                                           * 'AddLast' hace lo mismo pero al revez */
+            }
+            numeros.Remove(6);  //Removiendo el elemento '6' de la lista
+            foreach(int num in numeros)
+            {
+                Console.WriteLine(num);
+            }
+        
+            for(LinkedListNode<int> nodo = numeros.First; nodo != null; nodo = nodo.Next) /* Recorriendo  el array con objetos
+                                                                                           * de tipo nodo
+                                                                                           * (creando un nodo de nombre 'nodo'
+                                                                                           * inicializandolo con el primer valor
+                                                                                           * del array 'numeros.First'
+                                                                                           * mientras que el nodo sea diferente de nulo
+                                                                                           * el nodo en la otra vuelta del bucle va 
+                                                                                           * a ser el siguiente nodo 'nodo.next' */
+            {
+                int num = nodo.Value;        /* Inicializo una variable con el valor del nodo donde esta parado en ese momento el bucle
+                                              * 'nodo.Value' */
+                Console.WriteLine(num);       // Para asi imprimirlo en pantalla
+            }
+        }
+    }
+}
